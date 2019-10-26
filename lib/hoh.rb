778a695c5caf1f39a79414c07b83da789301a1b6
Olga  Chupp <olga.chupp@gmail.com>
2019-10-26 22:00:14 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,37 +1,37 @@
-Build a nested HoH of the classification of biological life on Earth based
-on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
-that HoH as the return value for the method "naming_system."
+# Build a nested HoH of the classification of biological life on Earth based
+# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
+# that HoH as the return value for the method "naming_system."
 
-The tests will guide your construction.
+# The tests will guide your construction.
 
-PORTION_1 = {
-  label: "Kingdom",
-  sub_category: {
-    label: "Phylum",
-    sub_category: {
-      label: "Class"
-    }
-  }
-}
+# PORTION_1 = {
+#   label: "Kingdom",
+#   sub_category: {
+#     label: "Phylum",
+#     sub_category: {
+#       label: "Class"
+#     }
+#   }
+# }
 
-PORTION_2 = {
-  label: "Order"
-}
+# PORTION_2 = {
+#   label: "Order"
+# }
 
-PORTION_3 = {
-  label: "Family",
-  sub_category: {
-    label: "Genus",
-  }
-}
+# PORTION_3 = {
+#   label: "Family",
+#   sub_category: {
+#     label: "Genus",
+#   }
+# }
 
-PORTION_4 = {
-  label: "Species",
-  sub_category: nil
-}
+# PORTION_4 = {
+#   label: "Species",
+#   sub_category: nil
+# }
 
 def naming_system
-  hoh => {{PORTION_1 = {
+  hoh => {{PORTION_1 => {
   label: "Kingdom",
   sub_category: {
     label: "Phylum",
@@ -39,14 +39,14 @@ def naming_system
       label: "Class"
     }
   }
-}},{PORTION_2 = {
+}},{PORTION_2=> {
   label: "Order"
-}},{PORTION_3 = {
+}},{PORTION_3 => {
   label: "Family",
   sub_category: {
     label: "Genus",
   }
-}},{PORTION_4 = {
+}},{PORTION_4 => {
   label: "Species",
   sub_category: nil
 }}}
